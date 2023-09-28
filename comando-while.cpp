ca0253a1189094dfd6a684c6c00fb5024b9d9257
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-09-28 19:04:50 -0300

    Add files via upload

---
+++ comando-while.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+	
+	n = 0;
+	
+	while (n < 10) {
+		cout << n << "\n";
+		n++;
+	}
+	
+	cout << "\n Rotina finalizada\n";
+	
+	return 0;
+}
